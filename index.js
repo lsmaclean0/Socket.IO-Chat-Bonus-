11989e6b65c60979d9cb095c71774dad6d707243
lsmaclean0 <36011983+lsmaclean0@users.noreply.github.com>
2018-02-06 16:42:42 -0500

    socket.IO chat edits
    adds ability to display user messages on the console

--- index.js
+++ index.js
@@ -8,9 +8,8 @@
  });
 
  io.on('connection', function(socket){
-	 console.log('a user connected');
-	 socket.on('disconnect', function(){
-		 console.log('user disconnected');
+	 socket.on('chat message', function(msg){
+		 console.log('message: ' + msg);
 	 });
  });
 
